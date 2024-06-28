468400cd7c57fa7e4d851861937120e0777c3850
RadheRavi3021 <radheravi01@gmail.com>
2024-06-28 19:19:47 +0530

    login page

---
+++ src/main/java/com/projects/springtodos/SpringTodosApplication.java
@@ -0,0 +1,13 @@
+package com.projects.springtodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTodosApplication.class, args);
+    }
+
+}
